001d5c731f83a544de5fee3dd4a8df89b5617043
Nick Telenson <ntelenson@gmail.com>
2019-08-28 12:36:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentence
     self.split(" ").count do |word|
-      word.sentence? || word.question?
+      word.sentence? || word.question? || word.exclamation?
     end
     
   end
