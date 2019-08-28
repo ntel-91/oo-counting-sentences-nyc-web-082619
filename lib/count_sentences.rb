8e9bb05608d70981f72273772529fb6f4e3b3b92
Nick Telenson <ntelenson@gmail.com>
2019-08-28 12:25:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    a = self.count(".")
+    b = self.count("!")
+    c = self.count("?")
+    (a + b + c)
   end
 end
