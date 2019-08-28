1aafe5cc553bf089cd7d9b057913cb47a6c2e709
Nick Telenson <ntelenson@gmail.com>
2019-08-28 12:47:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split.count do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end
+    
   end
 end
+
+# test = String.new 
+# test = "Hello.. I am here, I am here, I am here. Are you there?? Talk to me!!"
+# puts test.count_sentence
